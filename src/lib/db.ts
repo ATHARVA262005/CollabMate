d9c606c8405045532223c9a203257eeaf11020e5
Atharva262005 <atharvaralegankar2005@gmail.com>
2024-06-22 21:11:14 +0530

    auth commit

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,23 +1,23 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose, {Mongoose} from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL!;
+const MONGODB_URL=process.env.MONGODB_URL!;
 
 interface MongooseConn {
     conn: Mongoose | null;
     promise: Promise<Mongoose> | null; 
 }
 
-let cached: MongooseConn = (global as any).mongoose;
+let cached:any=(global as any).mongoose;
 
-if (!cached) {
-    cached = (global as any).mongoose = {
-        conn: null, 
-        promise: null
+if(!cached){
+    cached = (global as any).mongoose={
+        conn:null, 
+        promise:null
     };
 }
 
 export const connect = async () => {
-    if (cached.conn) {
+    if(cached.conn){
         return cached.conn;
     }
     
@@ -25,7 +25,7 @@ export const connect = async () => {
         dbName: "clerk-db",
         bufferCommands: false,
         connectTimeoutMS: 30000,
-    });
+    })
 
     cached.conn = await cached.promise;
     return cached.conn;
